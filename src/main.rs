2341f503711cf79e6171f932ff64cdf1d3b676e7
jan <jan.herschel@vision-robotics.de>
2024-01-19 19:30:12 +0100

    added error handling

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
+use std::fmt::format;
+use anyhow::{Context, Result};
 use clap::Parser;
 
 #[derive(Parser)]
-struct Cli{
+struct Cli {
     pattern: String,
     path: std::path::PathBuf,
 }
-
-
-fn main() {
-    
+fn main() -> Result<()>{
     let args = Cli::parse();
     println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 
+    let content = std::fs::read_to_string(&args.path)
+    .with_context(|| format!("could not read file: `{:?}`", &args.path))?;
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
 }
