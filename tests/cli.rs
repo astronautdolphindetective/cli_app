59e3628074f23d7e6ef1e392d92f5add1523c45b
jan <jan.herschel@vision-robotics.de>
2024-01-21 19:43:12 +0100

    tests are passing

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,3 +1,4 @@
+use assert_fs::prelude::*;
 use assert_cmd::prelude::*; // Add methods on commands
 use predicates::prelude::*; // Used for writing assertions
 use std::process::Command; // Run programs
@@ -13,3 +14,20 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = Command::cargo_bin("cli_app")?;
+    cmd.arg("test").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("A test\nAnother test"));
+
+    Ok(())
+}
+
